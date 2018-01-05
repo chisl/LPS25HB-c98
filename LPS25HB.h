a7c33bc2842a751109b2d39069df60fc65511273
chisl <info@chisl.io>
2018-01-05 21:17:24 +0100

    Initial commit

---
+++ LPS25HB.h
@@ -0,0 +1,15 @@
+/*
+ * name:        LPS25HB
+ * description: 260-1260 hPa absolute digital output barometer
+ * manuf:       STMicroelectronics
+ * version:     Version 0.1
+ * url:         http://www.st.com/resource/en/datasheet/lps25hb.pdf
+ * date:        2018-01-04
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+
